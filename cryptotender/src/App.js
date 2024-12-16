b7f6b5fab249b7dc6990d09b00cb2e966ea8ecf2
alexs <alex.sweet121@yahoo.com>
2024-12-16 10:40:04 +0000

    Fixed votes not displaying title in input field

--- cryptotender/src/App.js
+++ cryptotender/src/App.js
@@ -386,7 +386,7 @@ function App() {
               <select id="vote-tender-id" className="form-input">
                 {tenders.map((tender, index) => (
                   <option key={index} value={tender.id}>
-                    {tender.name} - Votes: {tender.votes}
+                    {tender.title} - Votes: {tender.votes.toString()}
                   </option>
                 ))}
               </select>
